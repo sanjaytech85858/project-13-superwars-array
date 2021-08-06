d5025739aa501ae965cbb7f1ebb166bb04883c2f
sanjaytech85858 <sanjaytech85858@gmail.com>
2021-08-06 22:43:32 +0530

    super3

--- src/app.js
+++ src/app.js
@@ -25,9 +25,19 @@ const PLAYERS = [
 const initPlayers = (players) => {
     let detailedPlayers = '';
 
+
+
     // Instead of forloop use Map method
     // Code here
 
+    detailedPlayers=players.map((player,i) => {
+        return {
+        name:player,
+        strength:getRandomStrength(),
+        image:"images/super-"+(i+1)+".png",
+        type:(i%2==0)? 'hero' : 'villain',
+        }
+    });  
     return detailedPlayers;
 }
 
@@ -39,6 +49,14 @@ const getRandomStrength = () => {
 // Build player template
 const buildPlayers = (players, type) => {
     let fragment = '';
+    fragment = players.filter(player => player.type == type).map(player => 
+        `<div class="player">
+        <img src="${player.image}" alt = "">
+        <div class="name">${player.name}</div>
+        <div class="strength">${player.strength}</div>
+        </div>`).join();
+
+
 
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
